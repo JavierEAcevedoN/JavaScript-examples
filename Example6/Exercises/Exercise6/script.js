91e790ca81b0d91f56a6bea128da9547f16674ee
JavierEAcevedoN <je.acevedonoguera2021@gmail.com>
2024-07-23 15:00:55 -0500

    feat: :sparkles: Agregado 1 ejercicio del ejemplo 6
    Agregado 1 ejercicio que falto de el ejemplo 6.
    
    by @JavierEAcevedoN

--- Example6/Exercises/Exercise6/script.js
+++ Example6/Exercises/Exercise6/script.js
@@ -1,18 +1,18 @@
 /* 
-find
+findIndex
 
-    -Crear una función find que acepte un array y un callback y que:
+    -Crear una función findIndex que acepte un array y un callback y que:
     -por cada elemento del array ejecute el callback pasándole dicho elemento como argumento
-    -devuelva el valor del elemento pasado como argumento del primer callback que devuelva true
+    -devuelva el índice del elemento pasado como argumento del primer callback que devuelva true
     -si ningún callback devuelve true, devuelva undefined
 */
 
-// Este es un modo de replicar el funcionamiento del metodo find().
+// Este es un modo de replicar el funcionamiento del metodo findIndex().
 findIndex = (numeros, multiploDe10) => {
     for (let numero = 0; numero < numeros.length; numero++) {
         const element = numeros[numero];
         if (multiploDe10(element)) {
-            console.log(element);
+            console.log(numero);
             return
         }
     }
@@ -23,4 +23,4 @@ const numeros = [8, 2, 3, 40, 33, 50]
     const multiploDe10 = x => x % 10 === 0
     findIndex(numeros, multiploDe10) // 3
 
-console.log(numeros.find(x => multiploDe10(x)));
+console.log(numeros.findIndex(x => multiploDe10(x)));
